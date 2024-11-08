0bb553f474dd617eea0684cdf1e414d49735cd54
zhannurr <154230915+zhannurr@users.noreply.github.com>
2024-11-08 20:12:57 +0500

    Adapetrrrrrrrr

---
+++ FinalProject/src/Internal/Adapter/OrderHistoryAdapter.java
@@ -0,0 +1,26 @@
+package Internal.Adapter;
+
+import Internal.Objects.Order;
+
+import javax.swing.*;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrderHistoryAdapter {
+    private List<Order> orders;
+
+    public OrderHistoryAdapter(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public void updateOrderHistoryDisplay(JTextArea textArea) {
+        StringBuilder displayText = new StringBuilder("История заказов:\n");
+        orders.sort(Comparator.comparing(Order::getDate).reversed());
+        for (Order order : orders) {
+            displayText.append("Заказ от ").append(order.getDate())
+                    .append(" - Сумма: ").append(order.calculateTotal()).append(" тг\n");
+        }
+        textArea.setText(displayText.toString());
+    }
+
+}
